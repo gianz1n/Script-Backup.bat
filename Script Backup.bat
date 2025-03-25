4d55acce24c9d723413d96357bd8ab470a916cc0
Gian Emanuel da Silva <contato.gianofc@gmail.com>
2025-03-25 09:40:18 -0300

    update code

--- Script Backup.bat
+++ Script Backup.bat
@@ -10,10 +10,7 @@ Date /t
 
 echo.
 
-xcopy "C:\Users\%username%\Desktop\*.*"  "D:\Bkp PC Portaria\Desktop" /D/E/C/I/K/O/Y
-xcopy "C:\Users\%username%\Downloads\*.*"  "D:\Bkp PC Portaria\Downloads" /D/E/C/I/K/O/Y
-xcopy "C:\Users\%username%\Pictures\*.*"  "D:\Bkp PC Portaria\Pictures" /D/E/C/I/K/O/Y
-xcopy "C:\Users\%username%\Documents\*.*"  "D:\Bkp PC Portaria\Docs" /D/E/C/I/K/O/Y
+robocopy "C:/Users/Gian/Desktop" "P:/Arquivos importantes/" /e
 
 echo.
 echo ----------Backup realizado com sucesso!----------
