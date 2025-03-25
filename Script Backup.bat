d7c63d3995104000d02abd48f564134d9808ff1d
Gian Emanuel da Silva <contato.gianofc@gmail.com>
2025-03-25 09:59:03 -0300

    commit

--- Script Backup.bat
+++ Script Backup.bat
@@ -1,18 +1,15 @@
 @echo off
 
 echo.
-echo ----------Iniciando Backup----------
-echo.
-
-Time /t
-Date /t
-
-echo.
-robocopy "C:/Users/Gian/Desktop" "P:/Arquivos importantes/" /e
+echo ---------------- [ Iniciando Backup ] ----------------
 echo.
 
 echo.
-echo ----------Backup realizado com sucesso!----------
+robocopy "P:/Arquivos" "C:/Users/Gian/Desktop/" /e
+echo.
+
+echo.
+echo ---------- [ Backup realizado com sucesso! ] ----------
 echo.
 
 pause
